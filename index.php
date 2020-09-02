51925c44a7fd6be480cb778c54a54da2fd20f525
manqsibeko1@gmail.com <manqsibeko1@gmail.com>
2020-09-02 11:07:48 +0200

    #

--- index.php
+++ index.php
@@ -4,6 +4,34 @@ require_once("include/DB.php");
 if(isset($_POST["submit"])){
     if(!empty($_POST["ename"]) &&!empty($_POST["ssn"]) ){
 
+        //inserting the data into the database 
+
+        $Ename = $_POST["ename"];
+        $SSN = $_POST["ssn"];
+        $Depart = $_POST["depart"];
+        $Salary = $_POST["salary"];
+        $H_Add = $_POST["homeaddress"];
+
+        global $connectingDB;
+        $sql = "INSERT INTO emp_record (ename,ssn,depart,salary,home_address) 
+        VALUES(:eName,:ssN,:depT,:salarY,:homeaddresS)";
+
+        // preparing our sql 
+        $stmt = $connectingDB->prepare($sql);
+
+        // defining these dummy names
+        $stmt ->bindValue(':eName',$Ename);
+        $stmt ->bindValue(':ssN',$SSN);
+        $stmt ->bindValue(':depT',$Depart);
+        $stmt ->bindValue(':salary',$Salary);
+        $stmt ->bindValue(':homeaddresS',$H_Add);
+
+        $Execute = $stmt->execute();
+
+        if   ($Execute){
+            echo "record is successfull";
+        }
+
     }else{
 
         echo "Please atleast add name and security number";
@@ -31,13 +59,13 @@ if(isset($_POST["submit"])){
             <input class="input" type="text" Name="ssn" value="" placeholder="Social Security Number"><br>
            
             <span>Department:</span><br>
-            <input class="input" type="text" Name="department" value="" placeholder="Department"><br>
+            <input class="input" type="text" Name="depart" value="" placeholder="Department"><br>
             
             <span>Salary:</span><br>
             <input class="input" type="text" Name="salary" value="" placeholder="salary"><br>
             
             <span>Home Address:</span><br>
-            <textarea Name="homeAddress" rows="5" cols="50" placeholder="Home Address" ></textarea><br>
+            <textarea Name="homeaddress" rows="5" cols="50" placeholder="Home Address" ></textarea><br>
                 <input class="submit" type="submit" Name="submit" value="Submit Your Information" placeholder="submit"><br>
         </fieldset>
     </form>
